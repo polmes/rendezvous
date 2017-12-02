26bc32b82e7e066bc1f6e9495f074b44711d4f53
polmes <pol.developer1@gmail.com>
2017-12-02 13:45:00 +0100

    Orbit class now using factory-pattern design

--- Orbit.m
+++ Orbit.m
@@ -10,34 +10,13 @@ classdef Orbit < matlab.mixin.Heterogeneous % to allow arrays of mixed classes
     
     methods
         % Constructor
-        function obj = Orbit(varargin)
-            % MATLAB is dumb
-            if nargin == 1 % (node)
-                node = varargin{1};
-                obj.sma = node.r;
-                % call rv2orbit with node.r and node.vi
-            elseif nargin == 6 % (sma, ecc, inc, arg, lan, mna)
-                obj.sma = varargin{1};
-                obj.ecc = varargin{2};
-                obj.inc = varargin{3};
-                obj.arg = varargin{4};
-                obj.lan = varargin{5};
-                obj.mna = varargin{6};
-            else
-                % Access error stack via MException.last
-                throw(MException('Orbit:Constructor:WrongNargin', ...
-                    'Invalid number of arguments when using Orbit constructor'));
-            end
-        end
-        
-        % Return position at a given time
-        function r = getPosition(t)
-            r = t;
-        end
-        
-        % Return velocity at a given time
-        function v = getVelocity(t)
-            v = t;
+        function obj = Orbit(sma, ecc, inc, arg, lan, mna)
+            obj.sma = sma;
+            obj.ecc = ecc;
+            obj.inc = inc;
+            obj.arg = arg;
+            obj.lan = lan;
+            obj.mna = mna;
         end
     end
 end
---
+++ OrbitFactory.m
@@ -0,0 +1,23 @@
+function orbit = OrbitFactory(varargin)
+    if nargin == 1 % (node)
+        node = varargin{1};
+        [sma, ecc, inc, arg, lan, mna] = util.rvt2orbit(node.r, noode.vi, node.t);
+    elseif nargin == 6 % (sma, ecc, inc, arg, lan, mna)
+        sma = varargin{1};
+        ecc = varargin{2};
+        inc = varargin{3};
+        arg = varargin{4};
+        lan = varargin{5};
+        mna = varargin{6};
+    else
+        throw(MException('OrbitFactory:WrongNargin', ...
+            'Invalid number of arguments when using OrbitFactory'));
+        % access error stack via MException.last
+    end
+    
+    if ecc < 1
+        orbit = EllipticOrbit(sma, ecc, inc, arg, lan, mna);
+    elseif ecc > 1
+        orbit = HyperbolicOrbit(sma, ecc, inc, arg, lan, mna);
+    end
+end
