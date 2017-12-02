4a57bb1aab9088450a9d3ab109e771af4748f1e9
polmes <pol.developer1@gmail.com>
2017-12-02 20:59:26 +0100

    Add gravity assist code for deltaV computation

--- PlanetNode.m
+++ PlanetNode.m
@@ -13,8 +13,26 @@ classdef PlanetNode < Node
         end
         
         function dv = computeDeltaV(obj)
-            % Gravity assist
-            dv = obj.vo - obj.vi + 1;
+            % Powered Gravity Assist
+            
+            % init
+            global options;
+            
+            % variables
+            vi_rel = vi - v_planet;
+            vo_rel = vo - v_planet;
+            ai = obj.mu/vi_rel^2;
+            ao = obj.mu/vo_rel^2;
+            delta = acos(dot(vi_rel, vo_rel) / (norm(vi_rel) * norm(vo_rel)));
+            
+            % periapsis
+            f = @(rpi) asin(ai/(rpi+ai)) + asin((ao/(rpi+ao))) - delta;
+            r_pi = fsolve(f, 2*r_pl,options);
+            
+            % deltaV
+            vi_pi = sqrt(vi_rel^2 + 2*mu_planet/r_pi);
+            vo_pi = sqrt(vo_rel^2 + 2*mu_planet/r_pi);
+            dv = abs(vo_pi - vi_pi);
         end
     end
 end
