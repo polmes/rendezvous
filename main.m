916d394b53aa81bacc99b1391864cffe16daec65
polmes <pol.developer1@gmail.com>
2017-12-02 19:43:42 +0100

    Init global constants at the beginning of the script

--- main.m
+++ main.m
@@ -1,3 +1,13 @@
+%% INIT
+
+global GM;
+G = 6.67408e-11; % Universal Gravitational Constant [m^3 / (kg * s^2)]
+M = 1.98855e+30; % Solar Mass [kg]
+GM = G * M;
+
+global options;
+options = optimset('Display', 'off'); % hide output from fsolve
+
 %% INPUT
 
 filename = 'input.mat';
@@ -8,7 +18,7 @@ solarSystem = util.readDatabase(database);
 
 %% OPTIMIZE DELTA V TOTAL
 
-initial = parameters; % keep initial guesses
+initial = parameters; % store initial guesses
 dV = computeDeltaV(nodes, parameters);
 
 %% OUTPUT
