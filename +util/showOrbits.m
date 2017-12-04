825ef9567177866c9d9cdefc640643fbc7cb379f
polmes <pol.developer1@gmail.com>
2017-12-04 23:20:31 +0100

    Add function to plot orbits

---
+++ +util/showOrbits.m
@@ -0,0 +1,31 @@
+function showOrbits(nodes)
+    N = 1:length(nodes);
+    NN = 100;
+    
+    figure;
+    hold('on');
+    
+    % Sun
+    scatter3(0, 0, 0, '*');
+    
+    for i = 1:N
+        r = nodes(i).r;
+        scatter3(r(1), r(2), r(3), 'filled');
+    end
+    
+    
+    for i = 1:N-1
+        orbit = OrbitFactory(nodes(i));
+        
+        rs = zeros(3, NN);
+        ts = linspace(nodes(i).t, nodes(i+1).t, NN);
+        for j = 1:NN
+            rs(j) = orbit.toIJK(ts(j));
+        end
+        
+        plot3(rs(1, :), rs(2, :); rs(3, :));
+    end
+    
+    grid('on');
+    axis('equal');    
+end
