03fe0dbfc5282bca29c437f00516ce6d9492fabe
polmes <pol.developer1@gmail.com>
2017-12-15 12:52:16 +0100

    Change dl when computeDeltaV crashes
    And stop if dV isn't reduced after 10 iterations

--- optimize.m
+++ optimize.m
@@ -1,18 +1,19 @@
-function [dV, nodes, parameters, grad, it] = optimize(nodes, parameters)
+function [mindV, nodes, dv, minParameters] = optimize(nodes, parameters)
     % Tolerance
     pardif = ones(size(parameters)) * 1;
-    dl = 10000;
-    epsi = 0.0002;
+    dl = 10000000;
+    epsi = 0.0005;
+    k = 10;
     
     % Ignore NaN
     nrm = @(x) norm(x(~isnan(x)));
     
     % Initial value
     dV = computeDeltaV(nodes, parameters);
-    
-%     figure;
-%     hold('on');
-%     colors = ['r' 'r' 'r' 'r'; 'g' 'b' 'y' 'm'; 'c' 'r' 'r' 'r'];
+
+    % Optimal
+    minParameters = parameters;
+    mindV = dV;
     
     it = 0;
     grad = parameters * Inf; % must be bigger than epsi
@@ -26,18 +27,29 @@ function [dV, nodes, parameters, grad, it] = optimize(nodes, parameters)
                 
                 test = parameters;
                 test(i, j) = test(i, j) + pardif(i, j);
-
+                
                 grad(i,j) = (computeDeltaV(nodes, test) - dV) / pardif(i,j);
-%                 plot(it, grad(i, j), ['x' colors(i, j)]);
-%                 pause(0.001);
             end
         end
-
-        parameters = parameters - grad * dl;
-        [dV, nodes] = computeDeltaV(nodes, parameters);
         
-%         plot(parameters(1, 1), dV, ['x' colors(3, 1)]);
-%         pause(0.001);
+        parameters = parameters - grad * dl;
+        if ~isnan(dV)
+            [dV, nodes, dv] = computeDeltaV(nodes, parameters);
+        else
+            warning('optimize:NaN', 'comuteDeltaV crashed. Reducing dl...');
+            parameters = minParameters;
+            [dV, nodes, dv] = computeDeltaV(nodes, parameters);
+            dl = dl / 10;
+            grad = parameters * Inf; % while check
+        end
+        
+        if dV < mindV
+            mindV = dV;
+            minParameters = parameters;
+            minIt = it;
+        elseif (it - minIt) > k
+            return;
+        end
         
         disp(['it = ' num2str(it) ' -> nrm(grad) = ' num2str(nrm(grad)) ' -> dV = ' num2str(dV / 1000) ' km/s']);
 
