c01541bd4fccfed9132996892b6fdff59d7316f0
polmes <pol.developer1@gmail.com>
2017-12-29 18:09:00 +0100

    Plot in [AU] instead of [m]

--- +util/showOrbits.m
+++ +util/showOrbits.m
@@ -2,6 +2,8 @@ function showOrbits(nodes)
     N = length(nodes);
     NN = 100;
     
+    au = 149597870700; % 1 [AU] in [m]
+    
     clf;
     hold('on');
     
@@ -10,6 +12,7 @@ function showOrbits(nodes)
     
     for i = 1:N
         r = nodes(i).r;
+        r = r / au;
         scatter3(r(1), r(2), r(3), 'filled');
     end
         
@@ -22,11 +25,16 @@ function showOrbits(nodes)
             rs(:, j) = orbit.toIJK(ts(j));
         end
         
+        rs = rs / au;
         plot3(rs(1, :), rs(2, :), rs(3, :));
     end
     
     nodeNames = 1:N;
     legend(["Sun"; string(num2str(nodeNames(:)))]);
+    
+    xlabel('$$x$$ [AU]', 'Interpreter', 'latex');
+    ylabel('$$y$$ [AU]', 'Interpreter', 'latex');
+    zlabel('$$z$$ [AU]', 'Interpreter', 'latex');
     
     grid('on');
     axis('equal');
